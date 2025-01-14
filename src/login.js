dd5f60bbbd72c0e93df251b878e1926070c0dc3e
Sean McNamee <seanmcnamee.45@gmail.com>
2025-01-13 23:59:22 -0500

    update to working end to end

--- src/login.js
+++ src/login.js
@@ -1,7 +1,8 @@
-const { By, Builder, Browser } = require('selenium-webdriver');
+const { By, Builder, until, Browser } = require('selenium-webdriver');
 const assert = require("assert");
 
 const config = require('config');
+let isFirstIteration = true;
 
 
 (async function firstTest() {
@@ -9,20 +10,29 @@ const config = require('config');
 
   try {
     driver = await new Builder().forBrowser(Browser.CHROME).build();
+    driver.manage().window().setRect({ width: 1280, height: 960, x: 0, y: 0 });
     await driver.get(config.get('website'));
-
     await driver.manage().setTimeouts({ implicit: 500 });
 
-    const navBurgerMenu = await driver.findElement(By.id('menu-button'));
-    await navBurgerMenu.click();
+    // const navBurgerMenu = await driver.findElement(By.id('menu-button'));
+    // await navBurgerMenu.click();
+
+    await new Promise(resolve => setTimeout(resolve, 500));
+
+    //// Login
+    const signInMenuLink = await driver.findElement(By.css("div#menu_myaccount a.menuitem"));
+    await signInMenuLink.click();
+
+    await new Promise(resolve => setTimeout(resolve, 10000));
 
     //// Navigate to Family Camping
     const headerMenuItems = await driver.findElements(By.css('#header-top li:has(.menuitem)'));
     const menuItemSearch = await findByText(headerMenuItems, 'SEARCH');
+    // await driver.wait(until.elementIsVisible(menuItemSearch), 2000);
 
     if (menuItemSearch) {
       const menuItemText = await menuItemSearch.getText();
-      console.log("Found! ", menuItemText);
+      console.log("Found menuItemSearch! ", menuItemText);
 
       await menuItemSearch.click();
     } else {
@@ -33,7 +43,7 @@ const config = require('config');
     const menuItemFamilyCamping = await findByText(subMenuItems, 'Family Camping');
     if (menuItemFamilyCamping) {
       const familyCampingText = await menuItemFamilyCamping.getText();
-      console.log("Found! ", familyCampingText);
+      console.log("Found menuItemFamilyCamping! ", familyCampingText);
 
       await menuItemFamilyCamping.click();
     } else {
@@ -41,8 +51,8 @@ const config = require('config');
     }
 
     //Apply filters
-    const filtersToggleButton = await driver.findElement(By.css('button.collapse-icon'));
-    await filtersToggleButton.click();
+    // const filtersToggleButton = await driver.findElement(By.css('button.collapse-icon'));
+    // await filtersToggleButton.click();
 
     //Begin Date    
     const beginDateToggle = await driver.findElement(By.id('begindate_vm_6_button'));
@@ -54,14 +64,14 @@ const config = require('config');
     const beginDateMonthOptions = await driver.findElements(By.css('button#begindate_vm_6_month_selection_button + div > ul > li'))
     const desiredMonth = await findByText(beginDateMonthOptions, config.get('filters.beginDate.month'));
     await desiredMonth.click();
-    
+
     //Begin Date - Day
     const beginDateDayToggle = await driver.findElement(By.css('button#begindate_vm_6_day_selection_button'));
     await beginDateDayToggle.click();
     const beginDateDayOptions = await driver.findElements(By.css('button#begindate_vm_6_day_selection_button + div > ul > li'))
     const desiredDay = await findByText(beginDateDayOptions, config.get('filters.beginDate.day'));
     await desiredDay.click();
-    
+
     //Begin Date - Year
     const beginDateYearToggle = await driver.findElement(By.css('button#begindate_vm_6_year_selection_button'));
     await beginDateYearToggle.click();
@@ -83,36 +93,109 @@ const config = require('config');
     //Park
     const parksOptions = await driver.findElements(By.css('#category_vm_2_wrap ul > li'));
     const desiredPark = await findByText(parksOptions, config.get('filters.park'));
+    driver.executeScript("arguments[0].scrollIntoView(true);", desiredPark);
+    await new Promise(resolve => setTimeout(resolve, 500));
     await desiredPark.click();
 
-    //Apply filters
-    const searchButton = await driver.findElement(By.css('button#rnwebsearch_buttonsearch'));
-    await searchButton.click();
+    //Site Search (keyword)
+    const siteSearchButton = await driver.findElement(By.css("div.newline.search-criteria.newline-fillin:has(label[for=keyword]) > button"));
+    await siteSearchButton.click();
+    const siteSearchInput = await driver.findElement(By.css("button.filter-collapsible.valid ~ div.field-wrap.fillin-field-wrap > input.fillin.character"));
+    driver.executeScript("arguments[0].setAttribute('value', arguments[1])", siteSearchInput, config.get('filters.siteSearch'));
 
-    //Diplay option
-    const displayTypeToggle = await driver.findElement(By.css('button#display_vm_5_button'));
-    await displayTypeToggle.click();
-    const displayTypeOptions = await driver.findElements(By.css('button#display_vm_5_button + div > ul > li'))
-    const desiredDisplayType = await findByText(displayTypeOptions, 'Graphical');
-    await desiredDisplayType.click();
-    
-    //Select first available of desired sites
-    const allSites = await driver.findElements(By.css('div.graphical-inner.ui-draggable.ui-draggable-handle > a'))
-    const allSitesAndDataTitle = await Promise.all(allSites.map(async site => ({
-      site: site,
-      dataTitle: await site.getAttribute("data-title")
-    })));
 
-    const desiredSiteStrList = config.get('desiredSiteList');
-    const desiredSites = desiredSiteStrList.map(desiredSiteStr => 
-      allSitesAndDataTitle.find(x => x.dataTitle.includes(desiredSiteStr))
-    );
+    //Loop control variables
+    let hasDesiredSiteSecuredInCart = false;
+    let hasAnyAvailableSites = false;
+    do {
+      //Apply filters
+      const searchButton = await driver.findElement(By.css('button#rnwebsearch_buttonsearch'));
+      await searchButton.click();
 
-    for (let desiredSite of desiredSites) {
-      //TODO: Check if "Status: Available" and click 
+      //Select first available of desired sites
+      await driver.wait(until.elementLocated(By.css('table#rnwebsearch_output_table')), 5000);
+      const allDesiredSiteRows = await driver.findElements(By.css('table#rnwebsearch_output_table > tbody > tr'));
+      // await driver.wait(until.elementIsVisible(allDesiredSiteRows), 5000);
+      // const allDesiredSiteCheckboxes = await driver.findElements(By.css('div.graphical-inner.ui-draggable.ui-draggable-handle > a'))
 
-      console.log("Desired Site: ", desiredSite.dataTitle);
+      const allDesiredSiteRowsAndProperties = await Promise.all(
+        allDesiredSiteRows.map(async row => ({
+          row: row,
+          checkbox: await row.findElement(By.css("td[data-title='Add to Cart'] > a")),
+          siteName: await (await row.findElement(By.css("td[data-title='Site/Item #']"))).getText()
+        }))
+      );
+
+      const desiredSiteStrList = config.get('desiredSiteList');
+      const desiredSites = desiredSiteStrList.map(desiredSiteStr =>
+        allDesiredSiteRowsAndProperties.find(x => x.siteName === desiredSiteStr)
+      );
+
+      hasDesiredSiteSecuredInCart = false;
+      hasAnyAvailableSites = false;
+      for (let desiredSite of desiredSites) {
+        const isAvailable = (await desiredSite.checkbox.getAttribute("data-tooltip")) === "Add To Selection List";
+        console.log("Desired Site: ", desiredSite.siteName, isAvailable);
+        if (isAvailable) {
+          hasAnyAvailableSites = true;
+          await desiredSite.checkbox.click();
+
+          //TODO: wait until 7pm
+          if (isFirstIteration) {
+            const hour = config.get("firstAttemptStartTime.hour");
+            const minute = config.get("firstAttemptStartTime.minute");
+            const second = config.get("firstAttemptStartTime.second");
+            await waitUntil(hour, minute, second);
+            isFirstIteration = false;
+          }
+
+          const addToCartButton = await driver.findElement(By.css("button.button.primary.multiselectlist__addbutton"));
+          await addToCartButton.click();
+
+          if (config.get("selectHouseHoldMember") === true) {
+            
+            await driver.wait(until.elementLocated(By.css('div.group.webaddtocartmatrix__membergroup div.field-wrap.checkbox-field-wrap:has(input.checkbox)')), 5000);
+            const familyMemberSelection = await driver.findElement(By.css("div.group.webaddtocartmatrix__membergroup div.field-wrap.checkbox-field-wrap:has(input.checkbox)"));
+            await familyMemberSelection.click();
+  
+            const continueCartButton = await driver.findElement(By.css("button#button201"));
+            await continueCartButton.click();
+          }
+
+          await driver.wait(until.elementLocated(By.css('#content h1.page-header')), 5000);
+          const pageHeader = await driver.findElement(By.css("#content h1.page-header"));
+          const pageHeaderText = await pageHeader.getText();
+
+          const buttonGroup = await driver.findElements(By.css("#processingprompts_buttongroup button, #processingprompts_buttongroup a"));
+          const continueSuccessButton = await findByText(buttonGroup, "Continue");
+
+          if (pageHeaderText.includes("(Purchase)") && continueSuccessButton !== undefined) {
+            hasDesiredSiteSecuredInCart = true;
+          } else {
+            const cancelCartButton = await driver.findElement(By.css("a#processingprompts_buttoncancel"));
+            await cancelCartButton.click();
+          }
+
+          break;
+        }
+      }
+    } while (!hasDesiredSiteSecuredInCart && hasAnyAvailableSites);
+
+
+    if (hasDesiredSiteSecuredInCart) {
+      console.log("Site secured in cart!");
     }
+    if (!hasAnyAvailableSites) {
+      console.log("Unable to find any desired site... Sorry");
+    }
+
+
+
+
+
+
+
+
 
     // let menuItemFamilyCamping;
     // for (let subMenuItem of subMenuItems) {
@@ -157,4 +240,13 @@ async function findByText(elements, text) {
       return el;
     }
   }
+}
+
+async function waitUntil(hours, minutes, seconds) {
+  const now = new Date();
+  const targetTime = new Date();
+  targetTime.setHours(hours, minutes, seconds, 0);
+  const delay = targetTime - now;
+
+  await new Promise(resolve => setTimeout(resolve, delay))
 }
